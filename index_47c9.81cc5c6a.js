59e41cc6658d9f42f61e0a1d3773c63d940d3c7c
niuiic <niuiic@users.noreply.github.com>
2023-11-07 06:04:27 +0000

    deploy: b90562520b2e2343e10d9a78d98f95f8414e4a0e

--- index_47c9.81cc5c6a.js
+++ index_47c9.81cc5c6a.js
@@ -4,6 +4,22 @@
         __farm_global_this__.__farm_module_system__.register(key, modules[key]);
     }
 })({
+    "092f3b3b": function(module, exports, farmRequire, farmDynamicRequire) {
+        "use strict";
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+        Object.defineProperty(exports, "default", {
+            enumerable: true,
+            get: function() {
+                return _default;
+            }
+        });
+        "";
+        var _default = {
+            "icon": `icon-0b50c465`
+        };
+    },
     "0c7ee846": function(module, exports, farmRequire, farmDynamicRequire) {
         "use strict";
         Object.defineProperty(exports, "__esModule", {
@@ -12,12 +28,75 @@
         var _export_star = farmRequire("@swc/helpers/_/_export_star");
         _export_star._(farmRequire("824607c6"), exports);
     },
+    "1e81784d": function(module, exports, farmRequire, farmDynamicRequire) {
+        "use strict";
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+        Object.defineProperty(exports, "Main", {
+            enumerable: true,
+            get: function() {
+                return Main;
+            }
+        });
+        var _interop_require_default = farmRequire("@swc/helpers/_/_interop_require_default");
+        var _jsxruntime = farmRequire("892c81fb");
+        var _input = farmRequire("6e9a280b");
+        var _mainmodulescss = _interop_require_default._(farmRequire("703aab8a"));
+        const Main = ()=>{
+            const searchChange = (value)=>{
+                console.log(value);
+            };
+            return (0, _jsxruntime.jsx)("div", {
+                className: _mainmodulescss.default.main,
+                children: (0, _jsxruntime.jsx)(_input.Input, {
+                    onChange: searchChange,
+                    placeholder: "Search Ideas"
+                })
+            });
+        };
+    },
+    "1fb2c2db": function(module, exports, farmRequire, farmDynamicRequire) {
+        "use strict";
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+        Object.defineProperty(exports, "Input", {
+            enumerable: true,
+            get: function() {
+                return Input;
+            }
+        });
+        var _interop_require_default = farmRequire("@swc/helpers/_/_interop_require_default");
+        var _jsxruntime = farmRequire("892c81fb");
+        var _icon = farmRequire("bc9e4543");
+        var _inputmodulescss = _interop_require_default._(farmRequire("8c2a1c95"));
+        const Input = (props)=>{
+            const onChange = (event)=>props.onChange && props.onChange(event.target?.value);
+            return (0, _jsxruntime.jsxs)("div", {
+                className: _inputmodulescss.default.input,
+                children: [
+                    (0, _jsxruntime.jsx)(_icon.Icon, {
+                        className: _inputmodulescss.default["input__prefix"],
+                        name: "search"
+                    }),
+                    (0, _jsxruntime.jsx)("input", {
+                        className: _inputmodulescss.default["input__inner"],
+                        type: "text",
+                        placeholder: props.placeholder,
+                        onChange: onChange
+                    })
+                ]
+            });
+        };
+    },
     "2c6e69f1": function(module, exports, farmRequire, farmDynamicRequire) {
         "use strict";
         Object.defineProperty(exports, "__esModule", {
             value: true
         });
         var _jsxruntime = farmRequire("892c81fb");
+        farmRequire("c5a7f327");
         "";
         "";
         "";
@@ -99,7 +178,7 @@
                     (0, _jsxruntime.jsxs)("p", {
                         className: _footermodulescss.default.text,
                         children: [
-                            "Access on ",
+                            "View on ",
                             (0, _jsxruntime.jsx)("span", {
                                 className: [
                                     _footermodulescss.default.link,
@@ -150,9 +229,34 @@
         });
         "";
         var _default = {
-            "footer": `footer-f2962e6e`,
-            "text": `text-f2962e6e`,
-            "link": `link-f2962e6e`
+            "footer": `footer-f568b156`,
+            "text": `text-f568b156`,
+            "bg-wave-flow": `bg-wave-flow-f568b156`,
+            "link": `link-f568b156`
+        };
+    },
+    "6e9a280b": function(module, exports, farmRequire, farmDynamicRequire) {
+        "use strict";
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+        var _export_star = farmRequire("@swc/helpers/_/_export_star");
+        _export_star._(farmRequire("1fb2c2db"), exports);
+    },
+    "703aab8a": function(module, exports, farmRequire, farmDynamicRequire) {
+        "use strict";
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+        Object.defineProperty(exports, "default", {
+            enumerable: true,
+            get: function() {
+                return _default;
+            }
+        });
+        "";
+        var _default = {
+            "main": `main-3d2210f3`
         };
     },
     "7dfa112b": function(module, exports, farmRequire, farmDynamicRequire) {
@@ -214,6 +318,24 @@
             });
         };
     },
+    "8c2a1c95": function(module, exports, farmRequire, farmDynamicRequire) {
+        "use strict";
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+        Object.defineProperty(exports, "default", {
+            enumerable: true,
+            get: function() {
+                return _default;
+            }
+        });
+        "";
+        var _default = {
+            "input": `input-f03885e4`,
+            "input__inner": `input__inner-f03885e4`,
+            "input__prefix": `input__prefix-f03885e4`
+        };
+    },
     "93843902": function(module, exports, farmRequire, farmDynamicRequire) {
         "use strict";
         Object.defineProperty(exports, "__esModule", {
@@ -229,6 +351,7 @@
         var _layout = farmRequire("7dfa112b");
         var _footer = farmRequire("9fd73c3e");
         var _nav = farmRequire("0c7ee846");
+        var _Main = farmRequire("1e81784d");
         const App = ()=>{
             const entries = [
                 "webgl",
@@ -241,9 +364,7 @@
                 nav: (0, _jsxruntime.jsx)(_nav.Nav, {
                     entries: entries
                 }),
-                main: (0, _jsxruntime.jsx)("div", {
-                    children: "hello"
-                }),
+                main: (0, _jsxruntime.jsx)(_Main.Main, {}),
                 footer: (0, _jsxruntime.jsx)(_footer.Footer, {})
             });
         };
@@ -269,13 +390,13 @@
         });
         "";
         var _default = {
-            "nav": `nav-fe2c2ca1`,
-            "title": `title-fe2c2ca1`,
-            "entry": `entry-fe2c2ca1`,
-            "slogan__inner": `slogan__inner-fe2c2ca1`,
-            "change-color": `change-color-fe2c2ca1`,
-            "slogan": `slogan-fe2c2ca1`,
-            "entries": `entries-fe2c2ca1`
+            "nav": `nav-562acdbd`,
+            "title": `title-562acdbd`,
+            "entry": `entry-562acdbd`,
+            "slogan__inner": `slogan__inner-562acdbd`,
+            "change-color": `change-color-562acdbd`,
+            "slogan": `slogan-562acdbd`,
+            "entries": `entries-562acdbd`
         };
     },
     "aedc679a": function(module, exports, farmRequire, farmDynamicRequire) {
@@ -291,6 +412,55 @@
         });
         var _default = "/JetBrainsMono-Regular.a9cb1cd8.woff2";
     },
+    "bc9e4543": function(module, exports, farmRequire, farmDynamicRequire) {
+        "use strict";
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+        var _export_star = farmRequire("@swc/helpers/_/_export_star");
+        _export_star._(farmRequire("e8703c4f"), exports);
+    },
+    "c5a7f327": function(module, exports, farmRequire, farmDynamicRequire) {
+        window._iconfont_svg_string_4319158 = '<svg><symbol id="icon-search" viewBox="0 0 1024 1024"><path d="M473.142857 54.514286c-231.2 0-418.742857 187.428571-418.742857 418.742857 0 231.2 187.428571 418.742857 418.742857 418.742857 102.742857 0 196.8-37.028571 269.6-98.285714L918.171429 969.142857l50.514285-50.514286-175.428571-175.657142c61.485714-72.914286 98.514286-167.085714 98.514286-269.828572 0.114286-231.2-187.428571-418.628571-418.628572-418.628571z m0 765.828571c-46.857143 0-92.342857-9.142857-135.085714-27.2-41.371429-17.485714-78.514286-42.514286-110.4-74.4-31.885714-31.885714-56.914286-69.028571-74.4-110.4-18.057143-42.742857-27.2-88.228571-27.2-135.085714s9.142857-92.342857 27.2-135.085714c17.485714-41.371429 42.514286-78.514286 74.4-110.4 31.885714-31.885714 69.028571-56.914286 110.4-74.4 42.742857-18.057143 88.228571-27.2 135.085714-27.2s92.342857 9.142857 135.085714 27.2c41.371429 17.485714 78.514286 42.514286 110.4 74.4 31.885714 31.885714 56.914286 69.028571 74.4 110.4 18.057143 42.742857 27.2 88.228571 27.2 135.085714s-9.142857 92.342857-27.2 135.085714c-17.485714 41.371429-42.514286 78.514286-74.4 110.4s-69.028571 56.914286-110.4 74.4c-42.742857 18.057143-88.228571 27.2-135.085714 27.2z" fill="#c7c9d3" ></path></symbol></svg>', function(n) {
+            var t = (t = document.getElementsByTagName("script"))[t.length - 1], e = t.getAttribute("data-injectcss"), t = t.getAttribute("data-disable-injectsvg");
+            if (!t) {
+                var o, i, c, s, d, a = function(t, e) {
+                    e.parentNode.insertBefore(t, e);
+                };
+                if (e && !n.__iconfont__svg__cssinject__) {
+                    n.__iconfont__svg__cssinject__ = !0;
+                    try {
+                        document.write("<style>.svgfont {display: inline-block;width: 1em;height: 1em;fill: currentColor;vertical-align: -0.1em;font-size:16px;}</style>");
+                    } catch (t) {
+                        console && console.log(t);
+                    }
+                }
+                o = function() {
+                    var t, e = document.createElement("div");
+                    e.innerHTML = n._iconfont_svg_string_4319158, (e = e.getElementsByTagName("svg")[0]) && (e.setAttribute("aria-hidden", "true"), e.style.position = "absolute", e.style.width = 0, e.style.height = 0, e.style.overflow = "hidden", e = e, (t = document.body).firstChild ? a(e, t.firstChild) : t.appendChild(e));
+                }, document.addEventListener ? ~[
+                    "complete",
+                    "loaded",
+                    "interactive"
+                ].indexOf(document.readyState) ? setTimeout(o, 0) : (i = function() {
+                    document.removeEventListener("DOMContentLoaded", i, !1), o();
+                }, document.addEventListener("DOMContentLoaded", i, !1)) : document.attachEvent && (c = o, s = n.document, d = !1, r(), s.onreadystatechange = function() {
+                    "complete" == s.readyState && (s.onreadystatechange = null, l());
+                });
+            }
+            function l() {
+                d || (d = !0, c());
+            }
+            function r() {
+                try {
+                    s.documentElement.doScroll("left");
+                } catch (t) {
+                    return void setTimeout(r, 50);
+                }
+                l();
+            }
+        }(window);
+    },
     "c711f3e9": function(module, exports, farmRequire, farmDynamicRequire) {
         "use strict";
         Object.defineProperty(exports, "__esModule", {
@@ -303,7 +473,32 @@
             }
         });
         var _default = "/JetBrainsMono-Bold.c503cc5e.woff2";
+    },
+    "e8703c4f": function(module, exports, farmRequire, farmDynamicRequire) {
+        "use strict";
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+        Object.defineProperty(exports, "Icon", {
+            enumerable: true,
+            get: function() {
+                return Icon;
+            }
+        });
+        var _interop_require_default = farmRequire("@swc/helpers/_/_interop_require_default");
+        var _jsxruntime = farmRequire("892c81fb");
+        var _iconmodulescss = _interop_require_default._(farmRequire("092f3b3b"));
+        const Icon = (props)=>(0, _jsxruntime.jsx)("svg", {
+                className: [
+                    _iconmodulescss.default.icon,
+                    props.className ?? ""
+                ].join(" "),
+                "aria-hidden": "true",
+                children: (0, _jsxruntime.jsx)("use", {
+                    xlinkHref: `#icon-${props.name}`
+                })
+            });
     }
 });
 
-//# sourceMappingURL=/index_d297.10fa6d61.js.map
+//# sourceMappingURL=/index_47c9.81cc5c6a.js.map
