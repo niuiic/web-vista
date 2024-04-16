879e7d262c3203143a95d4abde8191f8ae068d6a
niuiic <niuiicde@gmail.com>
2024-04-16 16:17:50 +0800

    feat(css): float

---
+++ packages/css/src/components/float/index.ts
@@ -0,0 +1,27 @@
+import sectionCss from '@/styles/section.scss'
+import html from './index.html?raw'
+import css from './index.scss'
+import scss from './index.scss?raw'
+
+const component = {
+  title: 'float',
+  desc: 'css float characterization',
+  category: 'css',
+  tags: ['standard'],
+  html,
+  css: css + sectionCss,
+  code: [
+    {
+      label: 'html',
+      filetype: 'html',
+      content: html
+    },
+    {
+      label: 'scss',
+      filetype: 'scss',
+      content: scss
+    }
+  ]
+}
+
+export default component
