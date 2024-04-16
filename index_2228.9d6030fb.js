c3d53c7910df8070f6aaac5e2609c2d09562dd59
niuiic <niuiic@users.noreply.github.com>
2024-04-16 01:57:28 +0000

    deploy: e8d2f90588d5db6ab66f04a04405a8bc7e95a906

--- index_2228.9d6030fb.js
+++ index_2228.9d6030fb.js
@@ -1,4 +1,4 @@
-(function(_){for(var r in _){_[r].__farm_resource_pot__='index_d121.js';(globalThis || window || global)['d1befa03c79ca0b84ecc488dea96bc68'].__farm_module_system__.register(r,_[r])}})({"016f013b":function  (module, exports, farmRequire, farmDynamicRequire) {
+(function(_){for(var r in _){_[r].__farm_resource_pot__='index_2228.js';(globalThis || window || global)['d1befa03c79ca0b84ecc488dea96bc68'].__farm_module_system__.register(r,_[r])}})({"016f013b":function  (module, exports, farmRequire, farmDynamicRequire) {
     (function(_) {
         for(var r in _){
             _[r].__farm_resource_pot__ = 'boxModel.js';
@@ -318,7 +318,7 @@ Padding, margin and border of the inline element would not push away surrounding
     farmRequire("5384d819");
     farmRequire("cf963046");
     farmRequire("016f013b");
-    farmRequire("d1a3929f");
+    farmRequire("b48914d1");
     farmRequire("b96493c0");
     farmRequire("63d7ee65");
     farmRequire("8e4d7570");
@@ -378,7 +378,7 @@ section h1 {
     (globalThis || window || global)['9a630259e374223af4de8e088557eb01'].__farm_module_system__.setInitialLoadedResources([
         'anonymousBlockBox.4b7cb13e.js',
         'boxModel.35d8e581.js',
-        'boxSize.703c980d.js',
+        'boxSize.e26d4008.js',
         'formattingContext.135e603f.js',
         'minContent.6d845daf.js',
         'wavyUnderline.13f8857c.js'
@@ -1761,6 +1761,266 @@ section h1 {
     const _default = "/JetBrainsMono-Regular.a9cb1cd8-9bdcf2.woff2";
 }
 ,
+"b48914d1":function  (module, exports, farmRequire, farmDynamicRequire) {
+    (function(_) {
+        for(var r in _){
+            _[r].__farm_resource_pot__ = 'boxSize.js';
+            (globalThis || window || global)['9a630259e374223af4de8e088557eb01'].__farm_module_system__.register(r, _[r]);
+        }
+    })({
+        "0878149f": function(o, e, d, i) {
+            "use strict";
+            Object.defineProperty(e, "__esModule", {
+                value: !0
+            }), Object.defineProperty(e, "default", {
+                enumerable: !0,
+                get: function() {
+                    return t;
+                }
+            });
+            let t = `.parent {
+  box-sizing: padding-box;
+  width: 100px;
+  height: 100px;
+  padding: 20px;
+  background-color: red;
+}
+
+.child {
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+}
+
+.parent2 {
+  width: auto;
+  height: 100px;
+  padding: 20px;
+  background-color: red;
+}
+
+.child2 {
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+}
+
+.parent3 {
+  height: auto;
+  padding: 20px;
+  background-color: red;
+}
+
+.child3 {
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+}
+
+.parent4 {
+  height: 0;
+  min-height: 100px;
+  padding: 20px;
+  background-color: red;
+}
+
+.child4 {
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+}
+
+.parent5 {
+  position: relative;
+  width: 100px;
+  padding: 20px;
+  background-color: red;
+}
+
+.child5 {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+}`;
+        },
+        "992f9bd0": function(e, t, i, o) {
+            "use strict";
+            Object.defineProperty(t, "__esModule", {
+                value: !0
+            }), Object.defineProperty(t, "default", {
+                enumerable: !0,
+                get: function() {
+                    return s;
+                }
+            });
+            let s = `<section>
+  <h1>
+    'width: 100%;' normally represents the proportion of the content area of the child box to the content area of the
+    container block. This would not be affected by box-sizing.
+  </h1>
+  <hr />
+
+  <div class="parent">
+    <div class="child"></div>
+  </div>
+</section>
+
+<section>
+  <h1>'width: 100%;' works even the value of width property of the container block is auto.</h1>
+  <hr />
+
+  <div class="parent2">
+    <div class="child2"></div>
+  </div>
+</section>
+
+<section>
+  <h1>'height: 100%;' does not work if the value of height property of the container block is auto.</h1>
+  <hr />
+
+  <div class="parent3">
+    <div class="child3"></div>
+    <span>block3</span>
+  </div>
+</section>
+
+<section>
+  <h1>'height: 100%;' is based on actual values, not set values.</h1>
+  <hr />
+
+  <div class="parent4">
+    <div class="child4"></div>
+    <span>block4</span>
+  </div>
+</section>
+
+<section>
+  <h1>
+    In absolute positioning, the reference is the padding box. It works even the value of height property of the
+    container block is auto.
+  </h1>
+  <hr />
+
+  <div class="parent5">
+    <div class="child5"></div>
+  </div>
+</section>
+`;
+        },
+        "e78c92ad": function(o, e, d, i) {
+            "use strict";
+            Object.defineProperty(e, "__esModule", {
+                value: !0
+            }), Object.defineProperty(e, "default", {
+                enumerable: !0,
+                get: function() {
+                    return t;
+                }
+            });
+            let t = `.parent {
+  box-sizing: padding-box;
+  width: 100px;
+  height: 100px;
+  padding: 20px;
+  background-color: red;
+}
+
+.child {
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+}
+
+.parent2 {
+  width: auto;
+  height: 100px;
+  padding: 20px;
+  background-color: red;
+}
+
+.child2 {
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+}
+
+.parent3 {
+  height: auto;
+  padding: 20px;
+  background-color: red;
+}
+
+.child3 {
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+}
+
+.parent4 {
+  height: 0;
+  min-height: 100px;
+  padding: 20px;
+  background-color: red;
+}
+
+.child4 {
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+}
+
+.parent5 {
+  position: relative;
+  width: 100px;
+  padding: 20px;
+  background-color: red;
+}
+
+.child5 {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+}
+`;
+        },
+        "f3e012b0": function(e, t, l, s) {
+            "use strict";
+            Object.defineProperty(t, "__esModule", {
+                value: !0
+            }), Object.defineProperty(t, "default", {
+                enumerable: !0,
+                get: function() {
+                    return n;
+                }
+            });
+            let a = l("@swc/helpers/_/_interop_require_default"), c = a._(l("d82e6e42")), d = a._(l("992f9bd0")), r = a._(l("0878149f")), f = a._(l("e78c92ad")), n = {
+                title: "box size",
+                desc: "css box size characterization",
+                category: "css",
+                tags: [
+                    "standard"
+                ],
+                html: d.default,
+                css: r.default + c.default,
+                code: [
+                    {
+                        label: "html",
+                        filetype: "html",
+                        content: d.default
+                    },
+                    {
+                        label: "scss",
+                        filetype: "scss",
+                        content: f.default
+                    }
+                ]
+            };
+        }
+    });
+}
+,
 "b96493c0":function  (module, exports, farmRequire, farmDynamicRequire) {
     (function(_) {
         for(var r in _){
@@ -2218,265 +2478,6 @@ section h1 {
     });
 }
 ,
-"d1a3929f":function  (module, exports, farmRequire, farmDynamicRequire) {
-    (function(_) {
-        for(var r in _){
-            _[r].__farm_resource_pot__ = 'boxSize.js';
-            (globalThis || window || global)['9a630259e374223af4de8e088557eb01'].__farm_module_system__.register(r, _[r]);
-        }
-    })({
-        "0878149f": function(o, e, d, i) {
-            "use strict";
-            Object.defineProperty(e, "__esModule", {
-                value: !0
-            }), Object.defineProperty(e, "default", {
-                enumerable: !0,
-                get: function() {
-                    return t;
-                }
-            });
-            let t = `.parent {
-  box-sizing: padding-box;
-  width: 100px;
-  height: 100px;
-  padding: 20px;
-  background-color: red;
-}
-
-.child {
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-}
-
-.parent2 {
-  width: auto;
-  height: 100px;
-  padding: 20px;
-  background-color: red;
-}
-
-.child2 {
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-}
-
-.parent3 {
-  height: auto;
-  padding: 20px;
-  background-color: red;
-}
-
-.child3 {
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-}
-
-.parent4 {
-  height: 0;
-  min-height: 100px;
-  padding: 20px;
-  background-color: red;
-}
-
-.child4 {
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-}
-
-.parent5 {
-  position: relative;
-  width: 100px;
-  height: 100px;
-  padding: 20px;
-  background-color: red;
-}
-
-.child5 {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-}`;
-        },
-        "992f9bd0": function(e, t, i, o) {
-            "use strict";
-            Object.defineProperty(t, "__esModule", {
-                value: !0
-            }), Object.defineProperty(t, "default", {
-                enumerable: !0,
-                get: function() {
-                    return s;
-                }
-            });
-            let s = `<section>
-  <h1>
-    'width: 100%;' normally represents the proportion of the content area of the child box to the content area of the
-    container block. This would not be affected by box-sizing.
-  </h1>
-  <hr />
-
-  <div class="parent">
-    <div class="child"></div>
-  </div>
-</section>
-
-<section>
-  <h1>'width: 100%;' works even the value of width property of the container block is auto.</h1>
-  <hr />
-
-  <div class="parent2">
-    <div class="child2"></div>
-  </div>
-</section>
-
-<section>
-  <h1>'height: 100%;' does not work if the value of height property of the container block is auto.</h1>
-  <hr />
-
-  <div class="parent3">
-    <div class="child3"></div>
-    <span>block3</span>
-  </div>
-</section>
-
-<section>
-  <h1>'height: 100%;' is based on actual values, not set values.</h1>
-  <hr />
-
-  <div class="parent4">
-    <div class="child4"></div>
-    <span>block4</span>
-  </div>
-</section>
-
-<section>
-  <h1>In absolute positioning, the reference is the padding box.</h1>
-  <hr />
-
-  <div class="parent5">
-    <div class="child5"></div>
-  </div>
-</section>
-`;
-        },
-        "e78c92ad": function(o, e, d, i) {
-            "use strict";
-            Object.defineProperty(e, "__esModule", {
-                value: !0
-            }), Object.defineProperty(e, "default", {
-                enumerable: !0,
-                get: function() {
-                    return t;
-                }
-            });
-            let t = `.parent {
-  box-sizing: padding-box;
-  width: 100px;
-  height: 100px;
-  padding: 20px;
-  background-color: red;
-}
-
-.child {
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-}
-
-.parent2 {
-  width: auto;
-  height: 100px;
-  padding: 20px;
-  background-color: red;
-}
-
-.child2 {
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-}
-
-.parent3 {
-  height: auto;
-  padding: 20px;
-  background-color: red;
-}
-
-.child3 {
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-}
-
-.parent4 {
-  height: 0;
-  min-height: 100px;
-  padding: 20px;
-  background-color: red;
-}
-
-.child4 {
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-}
-
-.parent5 {
-  position: relative;
-  width: 100px;
-  height: 100px;
-  padding: 20px;
-  background-color: red;
-}
-
-.child5 {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-}
-`;
-        },
-        "f3e012b0": function(e, t, l, s) {
-            "use strict";
-            Object.defineProperty(t, "__esModule", {
-                value: !0
-            }), Object.defineProperty(t, "default", {
-                enumerable: !0,
-                get: function() {
-                    return n;
-                }
-            });
-            let a = l("@swc/helpers/_/_interop_require_default"), c = a._(l("d82e6e42")), d = a._(l("992f9bd0")), r = a._(l("0878149f")), f = a._(l("e78c92ad")), n = {
-                title: "box size",
-                desc: "css box size characterization",
-                category: "css",
-                tags: [
-                    "standard"
-                ],
-                html: d.default,
-                css: r.default + c.default,
-                code: [
-                    {
-                        label: "html",
-                        filetype: "html",
-                        content: d.default
-                    },
-                    {
-                        label: "scss",
-                        filetype: "scss",
-                        content: f.default
-                    }
-                ]
-            };
-        }
-    });
-}
-,
 "e8703c4f":function  (module, exports, farmRequire, farmDynamicRequire) {
     "use strict";
     Object.defineProperty(exports, "__esModule", {
@@ -2696,4 +2697,4 @@ section h1 {
     _export_star._(farmRequire("2ea370be"), exports);
 }
 ,});
-//# sourceMappingURL=index_d121.2f1731a4.js.map
+//# sourceMappingURL=index_2228.9d6030fb.js.map
