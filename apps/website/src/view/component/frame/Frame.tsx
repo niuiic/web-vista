009aab8d66873f99d230c214779491654fcbbe56
niuiic <niuiicde@gmail.com>
2024-05-18 15:51:15 +0800

    refactor: loading

--- apps/website/src/view/component/frame/Frame.tsx
+++ apps/website/src/view/component/frame/Frame.tsx
@@ -1,7 +1,5 @@
-import { classes } from '@/util/class'
 import { localUniqId } from '@/util/id'
 import { useEffect, useRef } from 'react'
-import cls from './frame.module.scss'
 
 interface Props {
   css?: string
@@ -53,5 +51,5 @@ export const Frame = (props: Props) => {
     }
   }, [props.js, props.html, props.css])
 
-  return <div id={id} ref={containerRef} className={classes(cls.frame, props.className)} />
+  return <div id={id} ref={containerRef} className={props.className} />
 }
