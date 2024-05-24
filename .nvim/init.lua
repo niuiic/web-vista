1c95837483aae96e015ea1d3ba9cf4b76bdb0f34
niuiic <niuiicde@gmail.com>
2024-05-24 14:41:54 +0800

    chore: tasks

--- .nvim/init.lua
+++ .nvim/init.lua
@@ -17,12 +17,56 @@ local task = require("task")
 local output = require("task.output")
 
 task.register({
-	name = "eslint fix",
+	name = "eslint fix all",
 	config = function()
 		return {
 			cmd = "pnpm",
 			args = { "eslint", "--fix", [[**/*.{js,ts,tsx}]] },
 		}
 	end,
-	on_exit = output.notify,
+	on_exit = output.notify_done,
+})
+
+task.register({
+	name = "format all",
+	config = function()
+		return {
+			cmd = "pnpm",
+			args = { "prettier", "-w", "**/*.(scss|css|ts|js|cjs|html|vue|frag|vert)" },
+		}
+	end,
+	on_exit = output.notify_done,
+})
+
+task.register({
+	name = "eslint fix current",
+	config = function()
+		return {
+			cmd = "pnpm",
+			args = { "eslint", "--fix", vim.api.nvim_buf_get_name(0) },
+		}
+	end,
+	on_exit = output.notify_done,
+})
+
+task.register({
+	name = "stylelint fix current",
+	config = function()
+		return {
+			cmd = "pnpm",
+			args = { "stylelint", "--fix", vim.api.nvim_buf_get_name(0) },
+		}
+	end,
+	on_exit = output.notify_done,
+})
+
+task.register({
+	name = "stylelint fix all",
+	config = function()
+		return {
+			cmd = "pnpm",
+			args = { "stylelint", "--fix", "**/*.(scss|css)" },
+		}
+	end,
+	on_exit = output.notify_done,
 })
--- .nvim/task/task.sh
+++
@@ -1,3 +0,0 @@
-if [ "$1"="format" ]; then
-	pnpm prettier -w "**/*.(scss|css|ts|js|cjs|html|vue|frag|vert)"
-fi
