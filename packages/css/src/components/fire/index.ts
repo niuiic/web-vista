de814327376e4b7853e26586547f634bdb0b883d
niuiic <niuiicde@gmail.com>
2024-05-26 11:17:36 +0800

    feat: fire

---
+++ packages/css/src/components/fire/index.ts
@@ -0,0 +1,27 @@
+import html from './index.html?raw'
+import css from './index.scss'
+import scss from './index.scss?raw'
+
+const component = {
+  id: 'fire',
+  title: 'fire',
+  desc: 'Fire animation',
+  category: 'css',
+  tags: ['animation'],
+  html,
+  css,
+  code: [
+    {
+      label: 'html',
+      filetype: 'html',
+      content: html
+    },
+    {
+      label: 'scss',
+      filetype: 'scss',
+      content: scss
+    }
+  ]
+}
+
+export default component
